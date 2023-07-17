e7018d1a2ed6315375464443110b0ee572dcf4bd
tbagindo <77989568+tbagindo@users.noreply.github.com>
2023-07-18 02:58:33 +0700

    button

---
+++ ESP01-WiFi-Button/esp01-wifi-button-code/esp01-wifi-button-code.ino
@@ -0,0 +1,41 @@
+/* 
+ *  
+ *  ESP01 WiFi Remote sketch
+ */
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+IPAddress staIP(192,168,2,25);
+IPAddress gw(192,168,2,1);
+IPAddress mask(255,255,255,0);
+IPAddress dns(192,168,1,200);
+
+const char* ssid="ApaITU";
+const char* pass="1234567890";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.config(staIP,gw,mask,dns);
+  WiFi.begin(ssid,pass);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin("http://192.168.2.24/pumpON");
+    int httpCode = http.GET();
+    if(httpCode>0){
+      String payload = http.getString();
+      if(payload  == "ON"){
+        Serial.println("millis() : " + String(millis()));
+        delay(1000);
+        ESP.deepSleep(0);
+      }
+    }
+  }
+}
---
+++ ESP01-WiFi-Button/nodemcu_wifi_button_server/nodemcu_wifi_button_server.ino
@@ -0,0 +1,116 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+IPAddress staIP(192,168,2,24);
+IPAddress gw(192,168,2,1);
+IPAddress mask(255,255,255,0);
+IPAddress dns(192,168,1,200);
+
+const char* ssid = "ApaITU";
+const char* pass = "1234567890";
+
+static const char MAIN_PAGE[] PROGMEM = R"==(
+<html>
+<body>
+     <h1>wifi switch esp01</h1>
+    pump status  :  <span id="state"></span>
+</body>
+<script>
+     setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("state").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/pState", true);
+    xhttp.send();
+  }, 1000 ) ;
+</script>
+</html>
+)==";
+
+const int PUMP = D4;
+int pState = 0;
+uint32_t pMillis;
+int counter = 0;
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(PUMP,OUTPUT);
+    digitalWrite(PUMP, !pState);
+    WiFi.config(staIP,gw,mask,dns);
+    WiFi.begin(ssid,pass);
+    while(WiFi.status()!= 3){
+        Serial.print(".");
+        delay(1000);
+    }
+    server.on("/",indexHandler);
+    server.on("/pState",pumpState);
+    server.on("/pumpON",pumpON);
+    server.begin();
+    Serial.print("http://");
+    Serial.print(staIP);
+    Serial.println("/");
+    
+}
+
+void indexHandler(){
+    String page = FPSTR(MAIN_PAGE);
+    server.send(200,"text/html",page);
+    Serial.println("indexHandler");
+}
+
+void pumpState(){
+    static int pcount=0;
+    String state;
+    if(pState){
+        state ="ON";
+    }else{
+        state ="OFF";
+    }
+    server.send(200,"text/plain",state);
+    Serial.println("/pState : " + String(pcount++));
+}
+
+void pumpON(){
+    pState = 1;
+    digitalWrite(PUMP,!pState);
+    server.send(200,"text/plain","ON");
+    Serial.println("/pumpaON");
+}
+
+void loop(){
+    server.handleClient();
+
+    //=====START OF  DEMO
+    //demostration purpose only turn off pump after 6seconds
+    static bool cFlag;
+    uint32_t now = millis();
+    if(now-pMillis>1000){
+        pMillis = now;
+        if(pState){
+            if(!cFlag){
+                Serial.println("Start Counter");               
+                cFlag = true;  
+            }
+            counter++;      
+            if(counter>6){
+                counter = 0;
+                pState = 0;
+                digitalWrite(PUMP,!pState);
+                Serial.println("Reset Pump");
+                
+            }
+        }else{
+            if(cFlag){
+                cFlag = false;
+                counter = 0;
+            }
+        }
+           
+      }
+      // END OF DEMO ======
+    }
